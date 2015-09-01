031d1032de5283be6708ffc2c6f7eb0339cdf821
Evan <evan@datarank.com>
2015-08-31 23:02:15 -0500

    Began MultipleRegression and built out gradient_descent.py

---
+++ gradient_descent.py
@@ -0,0 +1,36 @@
+import random
+
+
+def in_random_order(data):
+    """generator that returns the elements of data in random order"""
+    indexes = [i for i, _ in enumerate(data)]
+    random.shuffle(indexes)
+    for i in indexes:
+        yield data[i]
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0 = 0.01):
+    data = zip(x, y)
+    theta = theta_0
+    alpha = alpha_0
+    min_theta, min_value = None, float("inf")
+    iterations_with_no_improvement = 0
+
+    # if 100 iterations with no improvement then stop
+    while iterations_with_no_improvement < 100:
+        value = sum( target_fn(x_i, y_i, theta) for x_i, y_i in data)
+
+        if value < min_value:
+            # if we found a new min then remember it
+            # and go back to original step size
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            # otherwise its not improving so shrink the step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        # and take a gradient step for each of the data points
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
