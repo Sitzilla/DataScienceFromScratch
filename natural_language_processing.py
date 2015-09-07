005d52c9a2c43559fcbbfa34465dae43c2b12d20
Evan <evan@datarank.com>
2015-09-07 17:41:47 -0500

    NLP: grammar rules logic

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -94,6 +94,51 @@ def n_gram_model():
     print generate_using_trigrams()
 
 
+def grammar_rules():
+    # data pasted from https://github.com/joelgrus/data-science-from-scratch/blob/master/code/natural_language_processing.py
+    grammar = {
+        "_S"  : ["_NP _VP"],
+        "_NP" : ["_N",
+                 "_A _NP _P _A _N"],
+        "_VP" : ["_V",
+                 "_V _NP"],
+        "_N"  : ["data science", "Python", "regression"],
+        "_A"  : ["big", "linear", "logistic"],
+        "_P"  : ["about", "near"],
+        "_V"  : ["learns", "trains", "tests", "is"]
+    }
+
+    def is_terminal(token):
+        return token[0] != "_"
+
+
+    def expand(grammar, tokens):
+        for i, token in enumerate(tokens):
+
+            # skip over terminals
+            if is_terminal(token): continue
+
+            # if we get here, we found a non-terminal token
+            # so we need to choose a replacement at random
+            replacement = random.choice(grammar[token])
+
+            if is_terminal(replacement):
+                tokens[i] = replacement
+            else:
+                tokens = tokens[:i] + replacement.split() + tokens[(i+1):]
+
+            # now call expand on new list of tokens
+            return expand(grammar, tokens)
+
+        # if we get here we had all terminals and are done
+        return tokens
+
+    def generate_sentence(grammar):
+        return expand(grammar, ["_S"])
+
+    print generate_sentence(grammar)
+
+grammar_rules()
 
 
 
@@ -102,4 +147,12 @@ def n_gram_model():
 
 
 
-n_gram_model()
+
+
+
+
+
+
+
+
+
