fc9d3c710fa177c22bdb16b6e5d588fef89edbf7
Evan <evan@datarank.com>
2015-09-07 11:47:23 -0500

    Added linear_algebra helper methods

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,10 +1,20 @@
+from __future__ import division
 from StdSuites import vectors
+import math
 
 
 def vector_add(v, w):
     """adds corresponding elements"""
     return [v_i + w_i
             for v_i, w_i in zip(v, w)]
+
+
+def vector_sum(vectors):
+    """sum all corresponding elements"""
+    result = vectors[0]
+    for vector in vectors[1:]:
+        result = vector_add(result, vector)
+    return result
 
 
 def vector_subtract(v, w):
@@ -21,3 +31,26 @@ def scalar_multiply(c, v):
 def dot(v, w):
     return sum(v_i * w_i
                for v_i, w_i in zip(v, w))
+
+
+def vector_mean(vectors):
+    """compute the vector whose ith element is the mean
+    of the ith elements of the input vectors"""
+    n = len(vectors)
+    return scalar_multiply(1 / n, vector_sum(vectors))
+
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
+
+def squared_distance(v, w):
+    """(v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2"""
+    return sum_of_squares(vector_subtract(v, w))
